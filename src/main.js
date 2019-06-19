a494949e84b32132814dcec8e9a7ffc91fa9eb6e
mila-zagrevskaya <zagrevskaya55@gmail.com>
2019-06-19 20:03:37 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Header from "./header";
 import Title from "./title"
 import About from "./section"
@@ -8,14 +8,24 @@ export default ({ header = true, children, footer }) => {
 	return (
 		<div className="container">
 			<div className="case">
+				{header && <Header />}	
 				<div className = "wrapper">
-					{header && <Header />}
 					<Title></Title>
 				</div>
 			</div>	
-			<div className="wrapper">
-				<About></About>
-			</div>
+			<div className = "section-wrap">
+				<div className="wrapper">
+					<About title = "About Amadeus Restaurant "
+						parag1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis lorem nec enim tempus viverra condimentum eget dui.  Quisque ac ornare orci lacinia facilisis velit "
+						span = "eget interdum ."
+						parag2 = "Quisque orci neque, euismod vitae turpis quis, accumsan iaculis velit. Nulla lacinia facilisis velit eget interdum."
+						btnText = "Read more"
+						bigImg = "./images/glassOfWine.jpg"
+					></About>
+				</div>				
+			</div>		
+
+
 				
 			<div className="main">{children}</div>
 
--- src/section.js
+++ src/section.js
@@ -1,30 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Button from "./button"
 
 
-const sectionArr = [
-    {
-        title: "About Amadeus Restaurant",
-        parag1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis lorem nec enim tempus viverra condimentum eget dui.  Quisque ac ornare orci lacinia facilisis velit eget interdum. ",
-        parag2: "Quisque orci neque, euismod vitae turpis quis, accumsan iaculis velit. Nulla lacinia facilisis velit eget interdum.",
-        btnText: "Read more",
-        bigImg: "./images/glassOfWine.jpg",
-    }
-]
 
-export default ({ text, ...rest }) => (
+
+export default ({ text, title, parag1, span, parag2, btnText, bigImg, ...rest }) => (
     <div className="section">
-        { sectionArr.map (el => 
-         <>
-            <div className="text-about">
-                <h2> {el.title}</h2>
-                <p>{ el.parag1} </p>
-                <p>{ el.parag2} </p>
-                <Button className="title-button" text={el.btnText} onClick="#"/>
-            </div>
-            <img src = {el.bigImg} alt="photo"/>
-         </>
-         )}
+        <div className="text-about">
+            <h2> {title}</h2>
+            <p>{ parag1} <span>{span}</span></p>
+            <p>{ parag2} </p>
+            <Button className="title-button more" text={btnText} onClick= "#"/>
+        </div>
+        <img src = {bigImg} className = "big-img" alt="photo"/>
     </div>
 )
